bc434880710562db86f201e4507bc23c996a82ad
=Jose Lambrechts <alt165@gmail.com>
2022-10-24 18:14:20 -0300

    captcha

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -73,3 +73,17 @@ inputs.forEach((input) => {
  input.addEventListener('keyup', validarFormulario); 
  input.addEventListener('blur', validarFormulario); 
 });
+$( '#btn-validate' ).click(function(){
+    var $captcha = $( '#recaptcha' ),
+        response = grecaptcha.getResponse();
+      if (response.length === 0) {
+      $( '.msg-error').text( "reCAPTCHA is mandatory" );
+      if( !$captcha.hasClass( "error" ) ){
+        $captcha.addClass( "error" );
+      }
+    } else {
+      $( '.msg-error' ).text('');
+      $captcha.removeClass( "error" );
+      alert( 'reCAPTCHA marked' );
+    }
+  })
--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -12,25 +12,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 6000); 
 } 
-
-function valida_envia() { 
-  //valido el nombre 
-  if (document.formulario.fname.value.length == 0) { 
-    alert("Ingrese su nombre") 
-    document.formulario.fname.focus() 
-    return 0; 
-  } 
- 
-  if (document.formulario.lname.value.length == 0) { 
-    alert("Ingrese su nombre") 
-    document.formulario.lname.focus() 
-    return 0; 
-  } 
-  if (document.formulario.email.value.length == 0) { 
-    alert("Ingrese su nombre") 
-    document.formulario.email.focus() 
-    return 0; 
-  } 
-  alert("Muchas gracias por enviar el formulario"); 
-  document.formulario.submit(); 
-}
